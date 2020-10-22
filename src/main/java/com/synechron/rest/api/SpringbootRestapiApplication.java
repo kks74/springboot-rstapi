5d7e506930316a8cef96404aec1bf86c56c9f5fe
kks74 <kaushal.kk@gmail.com>
2020-10-22 21:07:34 +0530

    Second Commit

--- src/main/java/com/synechron/rest/api/SpringbootRestapiApplication.java
+++ src/main/java/com/synechron/rest/api/SpringbootRestapiApplication.java
@@ -2,7 +2,7 @@ package com.synechron.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootRestapiApplication {
 
