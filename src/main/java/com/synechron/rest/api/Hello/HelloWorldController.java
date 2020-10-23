8754a355500fa7c86992e2225d8854cd439ae9d2
kks74 <kaushal.kk@gmail.com>
2020-10-23 10:28:21 +0530

    Hello World Base

---
+++ src/main/java/com/synechron/rest/api/Hello/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.synechron.rest.api.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+//	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Kaushal", "Singh", "Bokaro");
+	}
+
+}
